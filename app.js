a68efb0edb72757044456a6a59b007a4c3755c46
Arathi <93674779+shivaaru@users.noreply.github.com>
2022-07-13 17:19:54 +0530

    new change

--- app.js
+++ app.js
@@ -1,72 +1,149 @@
 const bodyParser = require('body-parser');
 const express = require("express");
+const bcrypt=require('bcryptjs');
 const session = require("express-session");
-const MongoDBStore = require("connect-mongodb-session")(session);
-const config = require("config");
-
-const appController = require("./controllers/appController");
-const isAuth = require("./middleware/is-auth");
-const connectDB = require("./config/db");
-const mongoURI = config.get("mongoURI");
+const MongoDBSession = require("connect-mongodb-session")(session);
+const mongoose = require("mongoose");
+const UserModel=require("./models/user");
+const Bookdata = require('./src/model/Bookdata');
 const app = express();
-connectDB();
+const mongoURI ='mongodb+srv://usertwo:usertwo@libraryapp.lswirdz.mongodb.net/LIBRARYAPP?retryWrites=true&w=majority';
 
-const store = new MongoDBStore({
-  uri: mongoURI,
-  collection: "mySessions",
-});
 
-const nav = [
-  {
-      link:'/books',name:'Books'
-  },
-  {
-      link:'/authors',name:'Authors'
-  },
-  {
-      link:'/admin',name:'Add Book'
-  },
-  {
-      link:'/admins',name:'Add Author'
-  },
- 
-];//
 
-app.set("view engine", "ejs");
+mongoose
+   .connect('mongodb+srv://usertwo:usertwo@libraryapp.lswirdz.mongodb.net/LIBRARYAPP?retryWrites=true&w=majority',{
+     useNewUrlParser: true,
+     useUnifiedTopology: true,
+   })
+   .then((res)=>{
+     console.log("MongoDB connected");
+   });
+  
+
+const store = new MongoDBSession({
+    uri: mongoURI,
+    collection:"mySessions",
+   });
+
+   const nav = [
+    {
+        link:'/books',name:'Books'
+    },
+    {
+        link:'/authors',name:'Authors'
+    },
+    {
+        link:'/admin',name:'Add Book'
+    },
+    {
+        link:'/admins',name:'Add Author'
+    },
+   
+   
+  ];//
+
+const authorRouter = require('./src/routes/authorRoutes')(nav);
+const booksRouter = require('./src/routes/bookRoutes')(nav);
+const adminRouter = require('./src/routes/adminRoutes')(nav);
+const adminRouter1 = require('./src/routes/adminRoutes1')(nav);
+const indexRouter = require('./src/routes/indexRouter')(nav);
+app.set("view engine","ejs");
 app.set('views','./src/views');
-app.use(express.static('./public'));
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended:true}));
+app.use(express.static('public'));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
+app.use('/authors',authorRouter);
+app.use('/books',booksRouter);
+app.use('/admin',adminRouter);
+app.use('/admins',adminRouter1);
+app.use('/users',booksRouter);
+app.use('/index',indexRouter);
+
+
+//app.get('/index',function(req,res){
+ // res.render("index",
+  // {
+  //    nav,
+  //    title:'Library'
+  // });
+  //});//
+  
+
+ 
+
+
 app.use(
-  session({
-    secret: "secret",
-    resave: false,
-    saveUninitialized: false,
-    store: store,
-  })
+    session({
+        secret: "key that will sign cookie",
+        resave: false,
+        saveUninitialized: false,
+        store:store,
+    })
 );
+const isAuth=(req,res,next)=>{
+ if(req.session.isAuth){
+  next()
+ }
+ else {
+  res.redirect('/login')
+ }
+}
+app.get("/",(req,res)=>{
+    res.render("landing");
+});
+app.get("/login",(req,res)=>{
+  res.render("login");
+});
+app.post("/login",async(req,res)=>{
+  const {email,password}=req.body;
+  const user=await UserModel.findOne({email});
+  if(!user){
+    return res.redirect('/login');
+  }
+  const isMatch=await bcrypt.compare(password,user.password);
+  if(!isMatch){
+    return res.redirect('/login');
+  }
+  req.session.isAuth=true;
+  res.redirect('/index');
+});
+app.get("/register",(req,res)=>{
+  res.render("register");
+});
+app.post("/register",async(req,res)=>{
+  const{username,email,password}=req.body;
+  let user = await UserModel.findOne({email})
+  if(user){
+    return res.redirect('/register');
+  }
+  const hashedPsw=await bcrypt.hash(password,12);
+  user=new UserModel({
+    username,
+    email,
+    password:hashedPsw
+  });
+  await user.save();
+  res.redirect('/login');
+});
+app.get("/dashboard",isAuth,(req,res)=>{
+  res.render("dashboard");
+});
+app.get("/index",isAuth,(req,res)=>{
+  res.render("index");
+});
+
+app.post('/logout',(req,res)=>{
+  req.session.destroy((err)=>{
+    if(err)throw err;
+    res.redirect("/");
+  });
+});
 
 
 
-//=================== Routes
-// Landing Page
-app.get("/", appController.landing_page);
 
-// Login Page
-app.get("/login", appController.login_get);
-app.post("/login", appController.login_post);
-
-// Register Page
-app.get("/register", appController.register_get);
-app.post("/register", appController.register_post);
-
-// Dashboard Page
-app.get("/index", isAuth, appController.dashboard_get);//
-
-app.post("/logout", appController.logout_post);
-
-
-
-app.listen(5000, console.log("App Running on http://localhost:5000"));
+app.listen(5000);
---
+++ src/views/server.js
@@ -0,0 +1,72 @@
+const bodyParser = require('body-parser');
+const express = require("express");
+const session = require("express-session");
+const MongoDBStore = require("connect-mongodb-session")(session);
+const config = require("config");
+
+const appController = require("./controllers/appController");
+const isAuth = require("./middleware/is-auth");
+const connectDB = require("./config/db");
+const mongoURI = config.get("mongoURI");
+const app = express();
+connectDB();
+
+const store = new MongoDBStore({
+  uri: mongoURI,
+  collection: "mySessions",
+});
+
+const nav = [
+  {
+      link:'/books',name:'Books'
+  },
+  {
+      link:'/authors',name:'Authors'
+  },
+  {
+      link:'/admin',name:'Add Book'
+  },
+  {
+      link:'/admins',name:'Add Author'
+  },
+ 
+];//
+
+app.set("view engine", "ejs");
+app.set('views','./src/views');
+app.use(express.static('./public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(
+  session({
+    secret: "secret",
+    resave: false,
+    saveUninitialized: false,
+    store: store,
+  })
+);
+
+
+
+//=================== Routes
+// Landing Page
+app.get("/", appController.landing_page);
+
+// Login Page
+app.get("/login", appController.login_get);
+app.post("/login", appController.login_post);
+
+// Register Page
+app.get("/register", appController.register_get);
+app.post("/register", appController.register_post);
+
+// Dashboard Page
+app.get("/index", isAuth, appController.dashboard_get);//
+
+app.post("/logout", appController.logout_post);
+
+
+
+app.listen(5000, console.log("App Running on http://localhost:5000"));
