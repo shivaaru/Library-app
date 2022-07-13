882910edc4bf5a324c2586ccae2ce86da7d18c34
Arathi <93674779+shivaaru@users.noreply.github.com>
2022-07-13 19:11:20 +0530

    new change

--- app.js
+++ app.js
@@ -146,4 +146,7 @@ app.post('/logout',(req,res)=>{
 
 
 
-app.listen(5000);
+const server = app.listen(process.env.PORT || 5000,() =>{
+  const port = server.address().port;
+  console.log(`Express is workingon port $"{port}`);
+});
